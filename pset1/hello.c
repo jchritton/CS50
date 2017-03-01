664684ea8aee0a161cdedfe89a7fa103e577f2b5
John <jchritton33@gmail.com>
2017-03-01 16:28:15 -0600

    Uploading hello.c 2017 edit
    Solution for CS50 Problem Set 1 "hello"

---
+++ pset1/hello.c
@@ -0,0 +1,14 @@
+/*
+    hello.c
+    
+    Author: John C, @JaxomofRuatha
+    
+    Solution for CS50 Problem Set 1 "hello"
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, world!\n");
+}
