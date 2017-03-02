47643d584b0d4fb2af5017e6009ac9d26518bcad
John <jchritton33@gmail.com>
2017-03-01 18:07:36 -0600

    Included generate.c file with comments
    File to generate pseudorandom numbers, included in download of find directory for CS50 Problem Set 3, comments added.

---
+++ pset3/generate.c
@@ -0,0 +1,55 @@
+/**
+ * generate.c
+ *
+ * Computer Science 50
+ * Problem Set 3
+ *
+ * Generates pseudorandom numbers in [0,LIMIT), one per line.
+ *
+ * Usage: generate n [s]
+ *
+ * where n is number of pseudorandom numbers to print
+ * and s is an optional seed
+ */
+ 
+#define _XOPEN_SOURCE
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// constant
+#define LIMIT 65536
+
+int main(int argc, string argv[])
+{
+    // Verify valid number of arguments
+    if (argc != 2 && argc != 3)
+    {
+        printf("Usage: generate n [s]\n");
+        return 1;
+    }
+
+    // Translate the first user entered argument into an integer
+    int n = atoi(argv[1]);
+
+    // If the user has input a second argument, use this as the seed for number generation
+    if (argc == 3)
+    {
+        srand48((long int) atoi(argv[2]));
+    }
+    else
+    {
+        srand48((long int) time(NULL));
+    }
+
+    // Print out the numbers generated, ensuring that they are less than the limit defined
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", (int) (drand48() * LIMIT));
+    }
+
+    // success
+    return 0;
+}
